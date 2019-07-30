4ff6ddb7736b211fff1f3dad2b29e717d3d5e948
Eli Peery <eli.g.peery@gmail.com>
2019-07-30 13:08:46 -0400

    Moved to src/Crux.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,6 @@
 module Main where
 
 import           Crux
-import           Crux.Actions.Utils ( fromMaybe )
-import           Crux.Browser
-import           Crux.Core
-import           Crux.Data
-import           Crux.Data.Types
-import           Crux.FS
 
 main :: IO ()
-main = do
-  s <- crux
-  writeCruxFile (CruxFile (fsCurrent . fromMaybe fsTop $ browserCursor $
-                           cruxBrowserState s)
-                          (cruxTodos s))
+main = crux
